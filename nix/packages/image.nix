a6a6c297a8681c9d2966a66966a71fcdf4c6763a
Luke Bailey <baileylu@tcd.ie>
2025-05-14 22:55:15 +0100

    Add oci container builder

---
+++ nix/packages/image.nix
@@ -0,0 +1,5 @@
+{ pkgs, flake, system }:
+pkgs.dockerTools.buildLayeredImage {
+  name = "nix_phoenix_template_image";
+  config.Cmd = [ "${flake.packages.${system}.default}/bin/nix_phoenix_template" ];
+}

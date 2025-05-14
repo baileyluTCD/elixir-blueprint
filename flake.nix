0435f50d172488d9a12c8728d8d021b003d820f7
Luke Bailey <baileylu@tcd.ie>
2025-05-14 20:56:50 +0100

    Setup elixir devshell

--- flake.nix
+++ flake.nix
@@ -1,13 +1,14 @@
 {
-  description = "Simple flake with a devshell";
+  description = "Elixir template project with nix build system";
 
-  # Add all your dependencies here
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     blueprint.url = "github:numtide/blueprint";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  # Load the blueprint
-  outputs = inputs: inputs.blueprint { inherit inputs; };
+  outputs = inputs: inputs.blueprint { 
+    inherit inputs;
+    prefix = "nix/";
+  };
 }
